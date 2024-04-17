d2307d0b4bf5b683ca3397c0026ae38033a42d8e
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-17 16:08:34 +0200

    [fix] hidden style

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html lang="en" style={{ backgroundColor: '#171719' }}>
+		<Html lang="en" style={{ backgroundColor: '#171719', overflow: 'hidden' }}>
 			<Head />
 			<body>
 				<Main />
