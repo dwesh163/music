f3367b41403bf3a6eb4ebbbc8032dfa3fd5f97f2
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-18 21:27:30 +0200

    [add] Date

--- src/pages/api/playlist/[playlistId].js
+++ src/pages/api/playlist/[playlistId].js
@@ -38,7 +38,7 @@ export default async function Track(req, res) {
 			playlist.playlist = { name: playlistInfo.playlist_name, id: playlistInfo.public_id };
 			playlist.tracks = [];
 
-			const [tracksId] = await connection.execute('SELECT * FROM playlist_tracks WHERE playlist_id = ?', [playlistInfo.playlist_id]);
+			const [tracksId] = await connection.execute('SELECT * FROM playlist_tracks WHERE playlist_id = ? ORDER BY added_date', [playlistInfo.playlist_id]);
 
 			for (const trackId of tracksId) {
 				let [[track]] = await connection.execute('SELECT * FROM tracks WHERE track_id = ?', [trackId.track_id]);
@@ -53,6 +53,7 @@ export default async function Track(req, res) {
 				const [[album]] = await connection.execute('SELECT * FROM albums WHERE album_id = ?', [track.album_id]);
 
 				track.album = album;
+				track.date = trackId.added_date;
 				playlist.tracks.push(track);
 			}
 
@@ -61,8 +62,29 @@ export default async function Track(req, res) {
 			console.error(error);
 			res.status(500).json({ error: 'Internal Server Error' });
 		}
+	}
+	if (req.method === 'POST') {
+		const connection = await connectMySQL();
+
+		const { trackId } = JSON.parse(req.body);
+		const playlistId = req.query.playlistId;
+		const [[user]] = await connection.execute('SELECT * FROM users WHERE user_email = ?', [session.user.email]);
+		const [[playlistInfo]] = await connection.execute('SELECT * FROM playlists WHERE playlist_user = ? AND public_id = ?', [user.user_id, playlistId]);
+
+		if (!playlistInfo) {
+			return res.status(404).json({ error: 'Playlist not found' });
+		}
+
+		const [[track]] = await connection.execute('SELECT * FROM tracks WHERE track_public_id = ?', [trackId]);
+		const [[playlist_tracks]] = await connection.execute('SELECT * FROM playlist_tracks WHERE playlist_id = ? AND track_id = ?', [playlistInfo.playlist_id, track.track_id]);
+
+		if (playlist_tracks) {
+			res.status(403).json({ error: 'Invalid opration' });
+			return;
+		}
+
+		await connection.execute('INSERT INTO playlist_tracks (playlist_id, track_id, added_date) VALUES (?, ?, ?)', [playlistInfo.playlist_id, track.track_id, new Date()]);
 	} else {
-		res.setHeader('Allow', ['GET']);
 		res.status(405).json({ error: `La méthode ${req.method} n'est pas autorisée` });
 	}
 }
