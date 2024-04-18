bb6b86eafc81b395c0acb11c6b4f44dc050067b0
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-18 22:02:03 +0200

    [add] menu

--- src/pages/playlists/[playlistId].js
+++ src/pages/playlists/[playlistId].js
@@ -14,6 +14,12 @@ export default function Home() {
 	const [playlist, setPlaylist] = useState({});
 	const [songId, setSongId] = useState('');
 
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
 	useEffect(() => {
 		if (!router.query.playlistId) {
 			return;
@@ -46,18 +52,27 @@ export default function Home() {
 			</Head>
 			<main className="bg-[#171719]">
 				<Player songId={songId} setSongId={setSongId} playlist={playlist} />
+				<Menu isOpen={isOpen} setIsOpen={setIsOpen} />
 				<div className=" relative flex overflow-hidden bg-[#171719]">
-					<Menu />
 					{playlist.error ? (
 						<div className="h-[calc(100vh-101px)] w-full flex items-center justify-center text-white">Playlist not Found</div>
 					) : (
 						<div className="w-full h-full overflow-hidden">
-							<div className="w-full p-5 pl-4 sm:p-7 pb-0 sm:pb-0">
-								<h1 className="text-3xl mb-0 font-extrabold leading-none tracking-tight md:text-4xl lg:text-6xl text-white">{playlist.playlist.name}</h1>
-								<p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-									{playlist.tracks.length} Track{playlist.tracks.length > 1 ? 's' : ''}
-								</p>
+							<div className="w-full p-5 pl-4 sm:p-7 pb-0 sm:pb-0 flex justify-between">
+								<div>
+									<h1 className="text-3xl mb-0 font-extrabold leading-none tracking-tight md:text-4xl lg:text-6xl text-white">{playlist.playlist.name}</h1>
+									<p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+										{playlist.tracks.length} Track{playlist.tracks.length > 1 ? 's' : ''}
+									</p>
+								</div>
+
+								<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleMenu} className="flex-grow-0 flex-shrink-0 w-6 h-6 relative cursor-pointer sm:hidden" preserveAspectRatio="none">
+									<path d="M6.5 12C6.5 12.5523 6.05228 13 5.5 13C4.94772 13 4.5 12.5523 4.5 12C4.5 11.4477 4.94772 11 5.5 11C6.05228 11 6.5 11.4477 6.5 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
+									<path d="M13 12C13 12.5523 12.5523 13 12 13C11.4477 13 11 12.5523 11 12C11 11.4477 11.4477 11 12 11C12.5523 11 13 11.4477 13 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
+									<path d="M19.5 12C19.5 12.5523 19.0523 13 18.5 13C17.9477 13 17.5 12.5523 17.5 12C17.5 11.4477 17.9477 11 18.5 11C19.0523 11 19.5 11.4477 19.5 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
+								</svg>
 							</div>
+
 							<div className="sm:px-7 px-4 overflow-y-scroll overflow-x-hidden h-[calc(100vh-250px)]">
 								<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
 									<thead className="text-xs text-gray-900 uppercase dark:text-gray-400 w-full">
