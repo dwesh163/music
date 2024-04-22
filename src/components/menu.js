6ae4601dfc17b97b07bf4b758ae209346c5c4b4c
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-22 21:31:44 +0200

    [fix] Delete comments

--- src/components/menu.js
+++ src/components/menu.js
@@ -21,37 +21,6 @@ export default function Menu({ isOpen, setIsOpen }) {
 		{ name: 'Artists', svg: '/artists.svg', url: 'artists' },
 	]);
 
-	// useEffect(() => {
-	// 	const handleResize = () => {
-	// 		if (buttonRef.current) {
-	// 			const buttonPosition = buttonRef.current.getBoundingClientRect();
-	// 			const menuLeft = buttonPosition.left;
-	// 			const menuTop = buttonPosition.top + buttonPosition.height;
-
-	// 			setMenuPosition({ left: menuLeft, top: menuTop });
-	// 		}
-	// 	};
-
-	// 	window.addEventListener('resize', handleResize);
-	// 	handleResize();
-	// 	return () => {
-	// 		window.removeEventListener('resize', handleResize);
-	// 	};
-	// }, [isOpen]);
-
-	// useEffect(() => {
-	// 	const handleOutsideClick = (event) => {
-	// 		if (!buttonRef.current.contains(event.target)) {
-	// 			setIsOpen(false);
-	// 		}
-	// 	};
-
-	// 	window.addEventListener('click', handleOutsideClick);
-	// 	return () => {
-	// 		window.removeEventListener('click', handleOutsideClick);
-	// 	};
-	// }, [isOpen]);
-
 	useEffect(() => {
 		setIsLoading(true);
 		const fetchData = async () => {
@@ -93,81 +62,55 @@ export default function Menu({ isOpen, setIsOpen }) {
 	}
 
 	return (
-		<>
-			{/* <div className={'absolute' + (isOpen ? 'w-full h-full' : 'w-0 h-0')}>
-				{isOpen && (
-					<div className="z-30  divide-y absolute bg-[#1f1f1f] shadow w-44" style={{ top: menuPosition.top, left: menuPosition.left }}>
-						<ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
-							<li>
-								<a href="#" className="block px-4 py-2 dark:hover:bg-[#252525] dark:hover:text-white">
-									Reward
-								</a>
-							</li>
-							<li>
-								<a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-[#252525] dark:hover:text-white">
-									Promote
-								</a>
-							</li>
-							<li>
-								<p className="block px-4 py-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-[#252525] dark:hover:text-white">Create Playlist</p>
-							</li>
-						</ul>
-						<div className="py-1 text-sm">
-							<p onClick={() => signOut()} className="cursor-pointer block px-4 py-2 text-white hover:bg-gray-100 dark:hover:bg-[#252525] dark:hover:text-white">
-								Sign Out
-							</p>
-						</div>
-					</div>
-				)}
-			</div> */}
-
-			<div className={'lg:w-1/5 md:w-1/4 md:max-w-96 sm:w-[30%] h-full left-0 top-0 sm:relative absolute z-50 sm:z-10 overflow-hidden' + (isOpen ? '' : ' sm:flex hidden')}>
-				<div className="flex flex-col justify-start items-start w-full left-0 top-0 overflow-hidden gap-3 px-3 pt-4 pb-8 bg-[#212124] h-screen">
-					<div className="flex justify-between items-center flex-grow-0 flex-shrink-0 w-full relative p-3 pb-0 rounded-lg">
-						<div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-2.5 p-0.5 rounded-[999px] bg-[#63676f]">
+		<div className={'lg:w-1/5 md:w-1/4 md:max-w-96 sm:w-[30%] h-full left-0 top-0 sm:relative absolute z-50 sm:z-10 overflow-hidden' + (isOpen ? '' : ' sm:flex hidden')}>
+			<div className="flex flex-col justify-start items-start w-full left-0 top-0 overflow-hidden gap-3 px-3 pt-4 pb-8 bg-[#212124] h-screen">
+				<div className="flex justify-between items-center flex-grow-0 flex-shrink-0 w-full relative p-3 pb-0 rounded-lg">
+					<div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-2.5 rounded-[999px] bg-[#63676f]">
+						{session.user.image ? (
+							<img className="w-10 h-10 rounded-full" src={session.user.image} />
+						) : (
 							<p className="flex-grow-0 flex-shrink-0 w-8 h-8 text-8 pt-1 font-medium text-center text-[#cacad1]">
 								{session.user.name
 									.split(' ')
 									.map((word) => word[0])
 									.join('')}
 							</p>
-						</div>
-						<div>
-							{/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" ref={buttonRef} className="flex-grow-0 flex-shrink-0 w-6 h-6 relative cursor-pointer" preserveAspectRatio="none">
-								<path d="M6.5 12C6.5 12.5523 6.05228 13 5.5 13C4.94772 13 4.5 12.5523 4.5 12C4.5 11.4477 4.94772 11 5.5 11C6.05228 11 6.5 11.4477 6.5 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
-								<path d="M13 12C13 12.5523 12.5523 13 12 13C11.4477 13 11 12.5523 11 12C11 11.4477 11.4477 11 12 11C12.5523 11 13 11.4477 13 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
-								<path d="M19.5 12C19.5 12.5523 19.0523 13 18.5 13C17.9477 13 17.5 12.5523 17.5 12C17.5 11.4477 17.9477 11 18.5 11C19.0523 11 19.5 11.4477 19.5 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
-							</svg> */}
-						</div>
-					</div>
-					<div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0">
-						{menus.map((menu, index) => (
-							<div key={index + '-menu'} onClick={() => router.push('/')} className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-full relative gap-2.5 p-3 rounded-lg cursor-pointer">
-								<p className={'flex-grow w-full text-base font-medium text-left ' + (menu == selectedMenu ? 'text-[#0ff]' : 'text-[#fcfcfc]')}>{menu}</p>
-							</div>
-						))}
-					</div>
-					<div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative mt-5">
-						<p className="flex-grow-0 flex-shrink-0 w-[129px] h-4 text-xs font-medium text-left text-[#9898a6]">MY COLLECTION</p>
-						{collections.map((collection, index) => (
-							<div key={index + '-collection'} onClick={() => router.push('/' + collection.url)} className="flex justify-start items-center cursor-pointer flex-grow-0 flex-shrink-0 w-full relative gap-2.5 p-3 rounded-lg hover:text-gray-400 hover:brightness-75">
-								<div className="flex justify-start items-start flex-grow-0 flex-shrink-0 w-6 h-6 relative overflow-hidden gap-2.5">
-									<img src={collection.svg} />
-								</div>
-								<p className="flex-grow w-full text-sm font-medium text-left text-[#fcfcfc]">{collection.name}</p>
-							</div>
-						))}
-					</div>
-					<div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative">
-						<p className="flex-grow-0 flex-shrink-0 w-[129px] h-4 text-xs font-medium text-left text-[#9898a6]">MY PLAYLISTS</p>
-						{playlists.map((playlist, index) => (
-							<div key={index + '-playlist'} onClick={() => router.push('/playlists/' + playlist.id)} className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[216px] relative gap-2.5 p-3 rounded-lg cursor-pointer">
-								<p className="flex-grow w-48 text-sm font-medium text-left text-[#fcfcfc] hover:text-gray-400">{playlist.name}</p>
-							</div>
-						))}
+						)}
 					</div>
 				</div>
+				<div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0">
+					{menus.map((menu, index) => (
+						<div key={index + '-menu'} onClick={() => router.push('/')} className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-full relative gap-2.5 p-3 rounded-lg cursor-pointer">
+							<p className={'flex-grow w-full text-base font-medium text-left ' + (menu == selectedMenu ? 'text-[#0ff]' : 'text-[#fcfcfc]')}>{menu}</p>
+						</div>
+					))}
+				</div>
+				<div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative mt-5">
+					<p className="flex-grow-0 flex-shrink-0 w-[129px] h-4 text-xs font-medium text-left text-[#9898a6]">MY COLLECTION</p>
+					{collections.map((collection, index) => (
+						<div key={index + '-collection'} onClick={() => router.push('/' + collection.url)} className="flex justify-start items-center cursor-pointer flex-grow-0 flex-shrink-0 w-full relative gap-2.5 p-3 rounded-lg hover:text-gray-400 hover:brightness-75">
+							<div className="flex justify-start items-start flex-grow-0 flex-shrink-0 w-6 h-6 relative overflow-hidden gap-2.5">
+								<img src={collection.svg} />
+							</div>
+							<p className="flex-grow w-full text-sm font-medium text-left text-[#fcfcfc]">{collection.name}</p>
+						</div>
+					))}
+				</div>
+				<div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative">
+					<p className="flex-grow-0 flex-shrink-0 w-[129px] h-4 text-xs font-medium text-left text-[#9898a6]">MY PLAYLISTS</p>
+					{playlists.map((playlist, index) => (
+						<div
+							key={index + '-playlist'}
+							onClick={() => {
+								setIsOpen(false);
+								router.push('/playlists/' + playlist.id);
+							}}
+							className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[216px] relative gap-2.5 p-3 rounded-lg cursor-pointer">
+							<p className="flex-grow w-48 text-sm font-medium text-left text-[#fcfcfc] hover:text-gray-400">{playlist.name}</p>
+						</div>
+					))}
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
