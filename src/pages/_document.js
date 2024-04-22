986b245390d8956fe8490d0b5fea60a365986a98
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-22 21:28:46 +0200

    [add] PWA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en" style={{ backgroundColor: '#171719', overflow: 'hidden' }}>
-			<Head />
+			<Head>
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+				<meta name="apple-mobile-web-app-title" content="Musics" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<link rel="manifest" href="/manifest.json" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
