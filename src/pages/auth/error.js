284e18c76242573672906f9feaeaa64b277f0a5b
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-23 12:44:38 +0200

    [add] S in musics

--- src/pages/auth/error.js
+++ src/pages/auth/error.js
@@ -7,7 +7,7 @@ export default function Signin() {
 	return (
 		<div className="flex flex-col w-full items-center justify-center min-h-screen bg-[#171719]">
 			<div className="sm:w-full sm:max-w-sm p-8 w-full">
-				<h1 className="font-semibold rounded-lg focus:outline-none focus:shadow-outline flex items-center justify-center mb-10 tracking-tighter text-sky-600 text-5xl">MUSIC</h1>
+				<h1 className="font-semibold rounded-lg focus:outline-none focus:shadow-outline flex items-center justify-center mb-10 tracking-tighter text-sky-600 text-5xl">MUSICS</h1>
 				<h2 className="text-center sm:text-xl text-base text-white mb-8">
 					{router.query.error == 'AccessDenied' ? '' : 'An error occurred: '} {router.query.error}
 				</h2>
