349779821508413514d478ab8b7794da5fe452a7
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-25 18:58:00 +0200

    [fix] Don't limit artists name

--- src/pages/artists/[artistId].js
+++ src/pages/artists/[artistId].js
@@ -181,7 +181,7 @@ export default function PlayList() {
 															<p className="flex flex-wrap gap-1">
 																{track.artists.slice(0, 2).map((item, index) => (
 																	<span key={index}>
-																		<span className={'font-normal text-gray-100 cursor-pointer opacity-60 ' + (index !== 0 ? 'hidden sm:inline' : '')} onClick={() => router.push('/artists/' + item.id)}>
+																		<span className={'font-normal text-gray-100 cursor-pointer opacity-60'} onClick={() => router.push('/artists/' + item.id)}>
 																			{item.name}
 																			{index !== track.artists.slice(0, 2).length - 1 && <span className="visble">,</span>}
 																		</span>
