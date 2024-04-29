390780edaf976c9f28035397a551a9d7362c1cbb
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-29 09:27:33 +0200

    [fix] limit pages with player

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,16 @@ import 'tailwindcss/tailwind.css';
 import { SessionProvider } from 'next-auth/react';
 import Player from '@/components/player';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
 	const [isStarted, setIsStarted] = useState(false);
+	const router = useRouter();
+	const path = ['/', '/playlists', '/playlists/[playlistId]', '/albums', '/albums/[albumId]', '/tracks', '/artists', '/artists/[artistId]'];
 
 	return (
 		<SessionProvider session={session}>
-			<Player isStarted={isStarted} setIsStarted={setIsStarted} />
+			{path.includes(router.pathname) && <Player isStarted={isStarted} setIsStarted={setIsStarted} />}
 			<Component {...pageProps} isStarted={isStarted} setIsStarted={setIsStarted} />
 		</SessionProvider>
 	);
