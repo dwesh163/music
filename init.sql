f9bcc93983bab50d423c927ee7d6edb9f6e62ac2
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-29 15:37:07 +0200

    [fix] init.sql

--- init.sql
+++ init.sql
@@ -1,96 +1,95 @@
 -- Création de la table "users"
 CREATE TABLE IF NOT EXISTS users (
-                                     user_id INT NOT NULL UNIQUE AUTO_INCREMENT,
-                                     user_id_public VARCHAR(50) NOT NULL,
-                                     user_email VARCHAR(50) NOT NULL,
-                                     user_username VARCHAR(50) NOT NULL,
-                                     user_image VARCHAR(255) NOT NULL,
-                                     user_provider VARCHAR(30) NOT NULL,
-                                     user_company VARCHAR(30) NOT NULL,
-                                     user_name VARCHAR(150) NOT NULL,
-                                     user_version VARCHAR(10) NOT NULL,
-                                     user_access BOOLEAN NOT NULL,
-                                     PRIMARY KEY (user_id)
+    user_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    user_id_public VARCHAR(50) NOT NULL,
+    user_email VARCHAR(50) NOT NULL,
+    user_username VARCHAR(50) NOT NULL,
+    user_image VARCHAR(255) NOT NULL,
+    user_provider VARCHAR(30) NOT NULL,
+    user_company VARCHAR(30) NOT NULL,
+    user_name VARCHAR(150) NOT NULL,
+    user_version VARCHAR(10) NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
 -- Création de la table "artists"
 CREATE TABLE IF NOT EXISTS artists (
-                                       artist_id INT NOT NULL UNIQUE AUTO_INCREMENT,
-                                       artist_name VARCHAR(100) NOT NULL,
-                                       spotify_id VARCHAR(100) NOT NULL,
-                                       public_id VARCHAR(100) NOT NULL,
-                                       artist_type VARCHAR(100) NOT NULL,
-                                       country VARCHAR(50),
-                                       bio TEXT,
-                                       PRIMARY KEY (artist_id)
+    artist_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    artist_name VARCHAR(100) NOT NULL,
+    spotify_id VARCHAR(100) NOT NULL,
+    public_id VARCHAR(100) NOT NULL,
+    artist_type VARCHAR(100) NOT NULL,
+    country VARCHAR(50),
+    bio TEXT,
+    PRIMARY KEY (artist_id)
 );
 
 -- Création de la table "albums"
 CREATE TABLE IF NOT EXISTS albums (
-                                      album_id INT NOT NULL UNIQUE AUTO_INCREMENT,
-                                      album_name VARCHAR(100) NOT NULL,
-                                      album_image VARCHAR(255) NOT NULL,
-                                      album_type VARCHAR(100) NOT NULL,
-                                      spotify_id VARCHAR(100) NOT NULL,
-                                      public_id VARCHAR(100) NOT NULL,
-                                      release_date DATE,
-                                      total_tracks INT NOT NULL,
-                                      PRIMARY KEY (album_id)
+    album_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    album_name VARCHAR(100) NOT NULL,
+    album_image VARCHAR(255) NOT NULL,
+    album_type VARCHAR(100) NOT NULL,
+    spotify_id VARCHAR(100) NOT NULL,
+    public_id VARCHAR(100) NOT NULL,
+    release_date DATE,
+    total_tracks INT NOT NULL,
+    PRIMARY KEY (album_id)
 );
 
 -- Création de la table "tracks"
 CREATE TABLE IF NOT EXISTS tracks (
-                                      track_id INT NOT NULL UNIQUE AUTO_INCREMENT,
-                                      name VARCHAR(100) NOT NULL,
-                                      duration INT,
-                                      album_id INT REFERENCES albums(album_id),
-                                      disc_number INT,
-                                      track_number INT,
-                                      popularity INT,
-                                      spotify_id VARCHAR(100),
-                                      track_public_id VARCHAR(100),
-                                      release_date DATE,
-                                      PRIMARY KEY (track_id)
+    track_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    duration INT,
+    album_id INT REFERENCES albums(album_id),
+    disc_number INT,
+    track_number INT,
+    popularity INT,
+    spotify_id VARCHAR(100),
+    track_public_id VARCHAR(100),
+    release_date DATE,
+    PRIMARY KEY (track_id)
 );
 
 -- Création de la table "playlists"
 CREATE TABLE IF NOT EXISTS playlists (
-                                         playlist_id INT NOT NULL UNIQUE AUTO_INCREMENT,
-                                         playlist_name VARCHAR(100) NOT NULL,
-                                         public_id VARCHAR(100),
-                                         playlist_user INT REFERENCES users(user_id),
-                                         PRIMARY KEY (playlist_id)
+    playlist_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    playlist_name VARCHAR(100) NOT NULL,
+    public_id VARCHAR(100),
+    playlist_user INT REFERENCES users(user_id),
+    PRIMARY KEY (playlist_id)
 );
 
 -- Création de la table de relation "playlist_tracks"
 CREATE TABLE IF NOT EXISTS playlist_tracks (
-                                               playlist_id INT REFERENCES playlists(playlist_id),
-                                               track_id INT REFERENCES tracks(track_id),
-                                               added_date DATETIME NOT NULL,
-                                               PRIMARY KEY (playlist_id, track_id)
+    playlist_id INT REFERENCES playlists(playlist_id),
+    track_id INT REFERENCES tracks(track_id),
+    added_date DATETIME NOT NULL,
+    PRIMARY KEY (playlist_id, track_id)
 );
 -- Création de la table de relation "album_artist"
 CREATE TABLE IF NOT EXISTS album_artist (
-                                            album_id INT REFERENCES albums(album_id),
-                                            artist_id INT REFERENCES artists(artist_id),
-                                            PRIMARY KEY (album_id, artist_id)
+    album_id INT REFERENCES albums(album_id),
+    artist_id INT REFERENCES artists(artist_id),
+    PRIMARY KEY (album_id, artist_id)
 );
 
 -- Création de la table de relation "track_artist"
 CREATE TABLE IF NOT EXISTS track_artist (
-                                            track_id INT REFERENCES tracks(track_id),
-                                            artist_id INT REFERENCES artists(artist_id),
-                                            PRIMARY KEY (track_id, artist_id)
+    track_id INT REFERENCES tracks(track_id),
+    artist_id INT REFERENCES artists(artist_id),
+    PRIMARY KEY (track_id, artist_id)
 );
 
 CREATE TABLE IF NOT EXISTS comments (
-                                            comments_id INT NOT NULL UNIQUE AUTO_INCREMENT,
-                                            Comments_title VARCHAR(255) NOT NULL,
-                                            Comments_text TEXT NOT NULL,
-                                            comments_public_id VARCHAR(100),
-                                            comments_date DATETIME,
-                                            Comments_user INT REFERENCES users(user_id),
-                                            PRIMARY KEY (comments_id)
+    comments_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    Comments_title VARCHAR(255) NOT NULL,
+    Comments_text TEXT NOT NULL,
+    comments_public_id VARCHAR(100),
+    comments_date DATETIME,
+    Comments_user INT REFERENCES users(user_id),
+    PRIMARY KEY (comments_id)
 );
 
 CREATE VIEW app_info AS
