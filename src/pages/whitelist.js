84122ae1fbb1cb41f36c89ccb2f66b10cc7da4bb
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-02 19:09:24 +0200

    [add] Auto refresh

--- src/pages/whitelist.js
+++ src/pages/whitelist.js
@@ -12,6 +12,7 @@ export default function PlayList({ isStarted, setIsStarted }) {
 	const router = useRouter();
 
 	const [isLoading, setIsLoading] = useState(true);
+	const [isContentLoading, setIsContentLoading] = useState(true);
 	const [whitelist, setWhitelist] = useState([]);
 	const [selectedUser, setSelectedUser] = useState(null);
 
@@ -55,6 +56,7 @@ export default function PlayList({ isStarted, setIsStarted }) {
 	};
 
 	const fetchData = async () => {
+		setIsContentLoading(true);
 		fetch('/api/admin/whitelist')
 			.then((response) => {
 				if (!response.ok) {
@@ -69,7 +71,10 @@ export default function PlayList({ isStarted, setIsStarted }) {
 				});
 				setWhitelist(whitelistData);
 				setSelectedAuthorizations(parsedData);
-				setIsLoading(false);
+				setTimeout(() => {
+					setIsContentLoading(false);
+					setIsLoading(false);
+				}, 1000);
 			})
 			.catch((error) => {
 				console.error('Error fetching playlist data:', error);
@@ -80,7 +85,7 @@ export default function PlayList({ isStarted, setIsStarted }) {
 		fetchData();
 		setInterval(() => {
 			fetchData();
-		}, 10000);
+		}, 50000);
 	}, []);
 
 	useEffect(() => {
@@ -119,121 +124,137 @@ export default function PlayList({ isStarted, setIsStarted }) {
 						}}>
 						<div className="w-full p-5 pl-4 sm:p-7 pb-0 sm:pb-0 flex justify-between">
 							<h1 className="text-3xl mb-0 font-extrabold leading-none tracking-tight md:text-4xl lg:text-6xl text-white">Whitelist</h1>
-							<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleMenu} className="flex-grow-0 flex-shrink-0 w-6 h-6 relative cursor-pointer sm:hidden" preserveAspectRatio="none">
-								<path d="M6.5 12C6.5 12.5523 6.05228 13 5.5 13C4.94772 13 4.5 12.5523 4.5 12C4.5 11.4477 4.94772 11 5.5 11C6.05228 11 6.5 11.4477 6.5 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
-								<path d="M13 12C13 12.5523 12.5523 13 12 13C11.4477 13 11 12.5523 11 12C11 11.4477 11.4477 11 12 11C12.5523 11 13 11.4477 13 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
-								<path d="M19.5 12C19.5 12.5523 19.0523 13 18.5 13C17.9477 13 17.5 12.5523 17.5 12C17.5 11.4477 17.9477 11 18.5 11C19.0523 11 19.5 11.4477 19.5 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
-							</svg>
+							<div className="flex gap-2">
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" className="w-6 h-6 cursor-pointer" onClick={() => fetchData()} viewBox="0 0 16 16">
+									<path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
+									<path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+								</svg>
+								<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleMenu} className="flex-grow-0 flex-shrink-0 w-6 h-6 relative cursor-pointer sm:hidden" preserveAspectRatio="none">
+									<path d="M6.5 12C6.5 12.5523 6.05228 13 5.5 13C4.94772 13 4.5 12.5523 4.5 12C4.5 11.4477 4.94772 11 5.5 11C6.05228 11 6.5 11.4477 6.5 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
+									<path d="M13 12C13 12.5523 12.5523 13 12 13C11.4477 13 11 12.5523 11 12C11 11.4477 11.4477 11 12 11C12.5523 11 13 11.4477 13 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
+									<path d="M19.5 12C19.5 12.5523 19.0523 13 18.5 13C17.9477 13 17.5 12.5523 17.5 12C17.5 11.4477 17.9477 11 18.5 11C19.0523 11 19.5 11.4477 19.5 12Z" fill="#FCFCFC" stroke="#FCFCFC"></path>
+								</svg>
+							</div>
 						</div>
 
 						<div className="sm:px-7 px-4 mt-6 h-screen overflow-y-scroll">
-							<div className="w-full py-3 mb-[200px]">
-								<div className="relative overflow-x-auto">
-									<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-										<thead className="text-xs uppercase dark:text-gray-400">
-											<tr>
-												<th scope="col" className="px-3 sm:px-3 py-0.5 sm:py-1 text-center">
-													#
-												</th>
-												<th scope="col" className="sm:px-6 py-1 sm:py-3 sm:hidden table-cell">
-													User
-												</th>
-												<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
-													Name
-												</th>
-												<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
-													Email
-												</th>
-												<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
-													Authorization
-												</th>
-												<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
-													Username
-												</th>
-												<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
-													Provider
-												</th>
-												<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
-													Company
-												</th>
-												<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
-													Last Login
-												</th>
-											</tr>
-										</thead>
-										<tbody>
-											{whitelist.map((user, index) => (
-												<>
-													<tr key={index} className="bg-[#11111170] hover:bg-[#1d1d1d70] cursor-pointer text-xs sm:text-sm">
-														<th scope="row" className="px-3 sm:px-3 py-0.5 sm:py-1 font-medium whitespace-nowrap text-center">
-															{index + 1}
-															{whitelist.length}
-															{index}
-														</th>
-														<th scope="row" className=" sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap text-white sm:hidden table-cell" onClick={() => (!selectedUser || selectedUser != user.user_email ? setSelectedUser(user.user_email) : setSelectedUser(null))}>
-															<p className="text-base">{user.user_name}</p>
-															<p className={user.user_name ? 'text-gray-400 font-normal' : 'text-base'}>{user.user_email}</p>
-														</th>
-														<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap text-white hidden sm:table-cell">
-															{user.user_name}
-														</th>
-														<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap text-white hidden sm:table-cell">
-															{user.user_email}
-														</th>
-														<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap text-white">
-															{!user.isAdmin ? (
-																<select className="bg-transparent text-white border-none outline-none" value={selectedAuthorizations[user.user_id_public] || ''} onChange={(e) => handleAuthorizationChange(user.user_id_public, e.target.value)}>
-																	{authorizations.map((auth, index) => (
-																		<option key={index} value={auth}>
-																			{auth}
-																		</option>
-																	))}
-																</select>
-															) : (
-																'Admin'
-															)}
-														</th>
-														<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap hidden sm:table-cell">
-															{user.user_username}
-														</th>
-														<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap hidden sm:table-cell">
-															{user.user_provider}
-														</th>
-														<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap hidden sm:table-cell">
-															{user.user_company.slice(0, 1) === '@' ? user.user_company.slice(1) : user.user_company}
-														</th>
-														<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap hidden sm:table-cell">
-															{new Date(user.last_connect).toLocaleString('fr-FR')}
-														</th>
-													</tr>
-													{selectedUser == user.user_email && (
-														<tr className="bg-[#11111170] cursor-pointer" onClick={() => setSelectedUser(null)}>
-															<th scope="row" className={'px-2 pl-12' + (index + 1 == whitelist.length ? ' pb-5' : '')} colSpan="3">
-																<span className="flex gap-1">
-																	<p className="">Provider : </p>
-																	<p className="font-normal">{user.user_provider}</p>
-																</span>
-																<span className="flex gap-1">
-																	<p className="">Username : </p>
-																	<p className="font-normal">{user.user_username}</p>
-																</span>
-																<span className="flex gap-1">
-																	<p className="">Company : </p>
-																	<p className="font-normal">{user.user_company.slice(0, 1) === '@' ? user.user_company.slice(1) : user.user_company}</p>
-																</span>
-																<span className="flex gap-1">
-																	<p className="">Last Login : </p>
-																	<p className="font-normal">{new Date(user.last_connect).toLocaleString('fr-FR')}</p>
-																</span>
+							{!isContentLoading ? (
+								<div className="w-full py-3 mb-[200px]">
+									<div className="relative overflow-x-auto">
+										<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+											<thead className="text-xs uppercase dark:text-gray-400">
+												<tr>
+													<th scope="col" className="px-3 sm:px-3 py-0.5 sm:py-1 text-center">
+														#
+													</th>
+													<th scope="col" className="sm:px-6 py-1 sm:py-3 sm:hidden table-cell">
+														User
+													</th>
+													<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
+														Name
+													</th>
+													<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
+														Email
+													</th>
+													<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
+														Authorization
+													</th>
+													<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
+														Username
+													</th>
+													<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
+														Provider
+													</th>
+													<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
+														Company
+													</th>
+													<th scope="col" className="px-2 sm:px-6 py-1 sm:py-3 hidden sm:table-cell">
+														Last Login
+													</th>
+												</tr>
+											</thead>
+											<tbody>
+												{whitelist.map((user, index) => (
+													<>
+														<tr key={index} className="bg-[#11111170] hover:bg-[#1d1d1d70] cursor-pointer text-xs sm:text-sm">
+															<th scope="row" className="px-3 sm:px-3 py-0.5 sm:py-1 font-medium whitespace-nowrap text-center">
+																{index + 1}
+																{whitelist.length}
+																{index}
+															</th>
+															<th scope="row" className=" sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap text-white sm:hidden table-cell" onClick={() => (!selectedUser || selectedUser != user.user_email ? setSelectedUser(user.user_email) : setSelectedUser(null))}>
+																<p className="text-base">{user.user_name}</p>
+																<p className={user.user_name ? 'text-gray-400 font-normal' : 'text-base'}>{user.user_email}</p>
+															</th>
+															<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap text-white hidden sm:table-cell">
+																{user.user_name}
+															</th>
+															<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap text-white hidden sm:table-cell">
+																{user.user_email}
+															</th>
+															<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap text-white">
+																{!user.isAdmin ? (
+																	<select className="bg-transparent text-white border-none outline-none" value={selectedAuthorizations[user.user_id_public] || ''} onChange={(e) => handleAuthorizationChange(user.user_id_public, e.target.value)}>
+																		{authorizations.map((auth, index) => (
+																			<option key={index} value={auth}>
+																				{auth}
+																			</option>
+																		))}
+																	</select>
+																) : (
+																	'Admin'
+																)}
+															</th>
+															<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap hidden sm:table-cell">
+																{user.user_username}
+															</th>
+															<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap hidden sm:table-cell">
+																{user.user_provider}
+															</th>
+															<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap hidden sm:table-cell">
+																{user.user_company.slice(0, 1) === '@' ? user.user_company.slice(1) : user.user_company}
+															</th>
+															<th scope="row" className="px-2 sm:px-6 py-2 sm:py-4 font-medium whitespace-nowrap hidden sm:table-cell">
+																{new Date(user.last_connect).toLocaleString('fr-FR')}
 															</th>
 														</tr>
-													)}
-												</>
-											))}
-										</tbody>
-									</table>
+														{selectedUser == user.user_email && (
+															<tr className="bg-[#11111170] cursor-pointer" onClick={() => setSelectedUser(null)}>
+																<th scope="row" className={'px-2 pl-12' + (index + 1 == whitelist.length ? ' pb-5' : '')} colSpan="3">
+																	<span className="flex gap-1">
+																		<p className="">Provider : </p>
+																		<p className="font-normal">{user.user_provider}</p>
+																	</span>
+																	<span className="flex gap-1">
+																		<p className="">Username : </p>
+																		<p className="font-normal">{user.user_username}</p>
+																	</span>
+																	<span className="flex gap-1">
+																		<p className="">Company : </p>
+																		<p className="font-normal">{user.user_company.slice(0, 1) === '@' ? user.user_company.slice(1) : user.user_company}</p>
+																	</span>
+																	<span className="flex gap-1">
+																		<p className="">Last Login : </p>
+																		<p className="font-normal">{new Date(user.last_connect).toLocaleString('fr-FR')}</p>
+																	</span>
+																</th>
+															</tr>
+														)}
+													</>
+												))}
+											</tbody>
+										</table>
+									</div>
 								</div>
-							</div>
+							) : (
+								<div role="status" className="flex items-center justify-center h-screen h-[calc(100vh-100px)]">
+									<svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+										<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+									</svg>
+									<span className="sr-only">Loading...</span>
+								</div>
+							)}
 						</div>
 					</div>
 				</div>
