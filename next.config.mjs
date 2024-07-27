4f1a0f015842b92db860b2b6fb86fa8edba1daa5
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-07-27 19:44:56 +0200

    chore: Add lazy loading for images in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 import withPWAInit from '@ducanh2912/next-pwa';
-/** @type {import('next').NextConfig} */
 
+/** @type {import('next').NextConfig} */
 const withPWA = withPWAInit({
 	dest: 'public',
 	reloadOnOnline: true,
 	aggressiveFrontEndNavCaching: true,
 });
+
 const nextConfig = {
 	output: 'standalone',
+	images: {
+		domains: ['c.saavncdn.com', 'www.jiosaavn.com'], // Ajoutez les domaines autorisés ici
+	},
 };
 
 export default withPWA(nextConfig);
