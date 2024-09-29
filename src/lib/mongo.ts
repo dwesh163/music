5df7a18b0fc47b9eb40db9a4e350c23d17cbbb36
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-29 19:13:12 +0200

    Add mongo.ts file for connecting to MongoDB

---
+++ src/lib/mongo.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = `mongodb://${process.env.MONGO_USER_USERNAME}:${process.env.MONGO_USER_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
+//@ts-ignore
+let cached = global.mongoose;
+
+if (!cached) {
+	//@ts-ignore
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connect() {
+	if (cached.conn) {
+		return cached.conn;
+	}
+
+	if (!cached.promise) {
+		const opts = {
+			bufferCommands: false,
+		};
+
+		cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+			return mongoose;
+		});
+	}
+
+	cached.conn = await cached.promise;
+	return cached.conn;
+}
+
+async function disconnect() {
+	await mongoose.disconnect();
+}
+
+const db = { connect, disconnect };
+export default db;
