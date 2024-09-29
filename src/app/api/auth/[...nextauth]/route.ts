5546100d2e12b90933b8b8b4edd5da5c59c058bd
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-29 19:32:52 +0200

    Refactor auth.ts to use updated NextAuth signIn callback signature

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,9 @@
-import type { NextRequest } from 'next/server';
 import NextAuth from 'next-auth';
 import { authOptions } from '@/lib/auth';
+import type { NextRequest } from 'next/server';
 
-interface RouteHandlerContext {
-	params: { nextauth: string[] };
-}
-
-async function auth(req: NextRequest, context: RouteHandlerContext) {
-	return await NextAuth(req, context, authOptions);
+async function auth(req: NextRequest) {
+	return NextAuth(authOptions);
 }
 
 export { auth as GET, auth as POST };
