25210fa3b706d44071935987f262c37d926d88b1
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-31 19:55:09 +0100

    Revert "Add MongoDB initialization script"
    This reverts commit 46cd8d8a5de0d374cb1b2557d559fcab76425815.

--- mongo-init.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
-
-db.createCollection('users');
-
-db.createUser({
-	user: process.env.MONGO_INITDB_USER_USERNAME,
-	pwd: process.env.MONGO_INITDB_USER_PASSWORD,
-	roles: [{ role: 'readWrite', db: process.env.MONGO_INITDB_DATABASE }],
-});
