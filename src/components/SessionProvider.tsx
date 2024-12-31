c497a530bd19a3dd726fa6f1a9890112a3e97658
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-31 19:55:09 +0100

    Revert "Add SessionProvider component for managing user sessions"
    This reverts commit 87915189a291457ae9f611558e9399143a9621c3.

--- src/components/SessionProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import * as React from 'react';
-import { SessionProvider as NextAuthSessionProvider, type SessionProviderProps } from 'next-auth/react';
-
-export function SessionProvider({ children, session }: SessionProviderProps) {
-	return <NextAuthSessionProvider session={session}>{children}</NextAuthSessionProvider>;
-}
