3fca87157915522743ac6acbda232ed538ea7825
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-20 18:42:26 +0100

    Wrap children in SessionProvider to manage session state in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 export const dynamic = 'auto';
 import '@/global.css';
+import { SessionProvider } from '@/components/session-provider';
 import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,12 +17,12 @@ export const metadata: Metadata = {
 type RootLayoutProps = any;
 
 export default function RootLayout(props: RootLayoutProps) {
-	const { children } = props;
+	const { children, session } = props;
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head />
 			<body className={inter.className}>
-				{children}
+				<SessionProvider session={session}>{children}</SessionProvider>
 			</body>
 		</html>
 	);
