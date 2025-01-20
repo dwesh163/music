6bf90f9c71a1886620ff57e87dd8c56c321d0e1e
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-20 19:03:36 +0100

    Add RootLayout component to enforce user authentication on root route

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+import React from 'react';
+import { getUser } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+
+export default async function RootLayout({ children }: { children: ReactNode }) {
+	const user = await getUser();
+	if (!user) redirect('/login');
+
+	return <main>{children}</main>;
+}
