d51bc9a9c75c0a07588e9ff5426440ec1b5d5478
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-22 20:48:06 +0100

    chore: Remove unused files and components to clean up the codebase

--- src/app/(root)/search/layout.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import type React from 'react';
-
-export default function SearchLayout({ children }: React.PropsWithChildren) {
-	return children;
-}
--- src/components/player.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Player() {
-	return (
-		<section className="player">
-			<h1>Player</h1>
-		</section>
-	);
-}
