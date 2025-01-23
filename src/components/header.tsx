5b5d124358a89e347b9de52f0024884f4f2840ab
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-23 20:33:22 +0100

    Refactor Header component to implement debounced search and synchronize search input with URL

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,40 +1,52 @@
 'use client';
-
 import { Search } from 'lucide-react';
-import { User } from 'next-auth';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import { Input } from './ui/input';
 
-export default function TopBar({ user }: { user: User }) {
+function useDebouncedSearch(search: string, router: any) {
+	useEffect(() => {
+		if (!search) return;
+
+		const debounceTimer = setTimeout(() => {
+			const query = new URLSearchParams();
+			query.set('s', search);
+
+			router.push(`/search?${query.toString()}`);
+		}, 500);
+
+		return () => clearTimeout(debounceTimer);
+	}, [search, router]);
+}
+
+function useSyncSearchWithUrl(setSearch: (value: string) => void) {
+	useEffect(() => {
+		const url = new URL(window.location.href);
+		if (url.searchParams.has('s')) {
+			setSearch(url.searchParams.get('s') ?? '');
+		}
+	}, [setSearch]);
+}
+
+export function Header() {
 	const [search, setSearch] = useState('');
 	const router = useRouter();
+	const pathname = usePathname();
 
 	useEffect(() => {
-		const query = new URLSearchParams();
-		if (search) {
-			query.set('s', search);
-			router.replace(`/search?${query.toString()}`);
+		if (pathname !== '/search') {
+			setSearch('');
 		}
-	}, [search, router]);
+	}, [pathname]);
+
+	useDebouncedSearch(search, router);
+	useSyncSearchWithUrl(setSearch);
 
 	return (
-		<div className="flex items-center justify-between gap-4 mb-8">
+		<div className="flex items-center justify-between gap-4 py-6">
 			<div className="relative flex-1 w-full max-w-xl">
 				<Search className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
 				<Input placeholder="Search by artists, songs or albums" className="pl-9 h-10 w-full focus-visible:ring-2 bg-gray-800/50 rounded-full text-gray-300" value={search} onChange={(e) => setSearch(e.target.value)} />
-			</div>
-			<div className="flex items-center gap-4">
-				<Avatar className="border shadow-sm w-8 h-8 lg:w-10 lg:h-10">
-					<AvatarImage src={user.image} alt={user.name} />
-					<AvatarFallback>
-						{user.name
-							?.split(' ')
-							.map((word) => word.charAt(0).toUpperCase())
-							.join('')}
-					</AvatarFallback>
-				</Avatar>
 			</div>
 		</div>
 	);
