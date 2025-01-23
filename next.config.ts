73c0d7087b7989a75383f7ae790be1466fbe9450
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-23 18:24:48 +0100

    Add remote image patterns for Unsplash and Spotify in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	output: 'standalone',
+	images: {
+		remotePatterns: [
+			{ protocol: 'https', hostname: 'images.unsplash.com' },
+			{ protocol: 'https', hostname: 'i.scdn.co' },
+			{ protocol: 'https', hostname: 'mosaic.scdn.co' },
+			{ protocol: 'https', hostname: 'image-cdn-ak.spotifycdn.com' },
+		],
+	},
 };
 
 export default nextConfig;
