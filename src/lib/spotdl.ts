8823f0bcdb9c93025d63f03c10ed05b66d067ce6
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-25 16:10:33 +0100

    Add WebSocket client ID generation and update API URLs to use HTTP

--- src/lib/spotdl.ts
+++ src/lib/spotdl.ts
@@ -1,9 +1,28 @@
+import { SongModel } from '@/models/Song';
+import { ErrorType } from '@/types/error';
+import { v4 as uuidv4 } from 'uuid';
+
+export async function getClientId(): Promise<string | null> {
+	try {
+		const sessioinId = uuidv4();
+
+		const ws = new WebSocket(`ws://${process.env.API_HOST}/api/ws?client_id=${sessioinId}`);
+
+		await new Promise((resolve) => (ws.onopen = resolve));
+
+		return sessioinId;
+	} catch (error) {
+		console.error('Client ID fetch error:', error);
+		return null;
+	}
+}
+
 export async function downloadTrack(trackId: string): Promise<ErrorType> {
 	try {
 		const clientId = await getClientId();
 		if (!clientId) throw new Error('No valid client ID');
 
-		const response = await fetch(`${process.env.API_URL}/api/download/url?url=https:%2F%2Fopen.spotify.com%2Ftrack%2F${trackId}&client_id=${clientId}`, {
+		const response = await fetch(`http://${process.env.API_HOST}/api/download/url?url=https:%2F%2Fopen.spotify.com%2Ftrack%2F${trackId}&client_id=${clientId}`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -30,8 +49,9 @@ export async function getTrackFile(trackId: string): Promise<Blob | null> {
 	if (!clientId) throw new Error('No valid client ID');
 
 	const song = await SongModel.findOne({ id: trackId });
+	if (!song) return null;
 
-	const response = await fetch(`${process.env.API_URL}/api/download/file?file=${encodeURIComponent(song.path)}&client_id=${clientId}`);
+	const response = await fetch(`http://${process.env.API_HOST}/api/download/file?file=${encodeURIComponent(song.path)}&client_id=${clientId}`);
 
 	if (!response.ok) {
 		const data = await response.json();
