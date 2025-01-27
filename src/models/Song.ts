3b9e75692e9dbfd91934d5df251995899d8bb5d8
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-27 20:38:40 +0100

    Enhance Song model to include name, artists, album, and imageUrl fields

--- src/models/Song.ts
+++ src/models/Song.ts
@@ -4,11 +4,19 @@ export interface ISong extends Document {
 	_id: string;
 	id: string;
 	path: string;
+	name: string;
+	artists: String[];
+	album: String;
+	imageUrl: string;
 }
 
 const songSchema = new mongoose.Schema<ISong>({
 	id: { type: String, required: true },
 	path: { type: String, required: true },
+	name: { type: String, required: true },
+	artists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Artist' }],
+	album: { type: mongoose.Schema.Types.ObjectId, ref: 'Album' },
+	imageUrl: { type: String, required: true },
 });
 
 export const SongModel = mongoose.models.Song || mongoose.model<ISong>('Song', songSchema);
